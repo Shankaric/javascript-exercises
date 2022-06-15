ef90be89ab3916ee0608b629914b7866b74a2646
shankari <cshankari27@gmail.com>
2022-06-15 12:47:14 +0530

    tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
 const ftoc = function() {
-
+  let fahrenheit=32;
+  let ftoc=Math.floor(fahrenheit-32)*5/9;
+  return ftoc;
 };
 
 const ctof = function() {
-
+  let celcius=37.8;
+  let ctof=Math.floor(celcius*9/5)+32;
+  return ctof;
 };
 
 // Do not edit below this line
